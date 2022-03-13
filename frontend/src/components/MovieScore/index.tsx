e98e9e5b4b23a987f3e86a2767780d27ba2e8d77
Ricardo Lopes <prslopes1@gmail.com>
2022-03-13 00:31:50 -0300

    Show score

--- frontend/src/components/MovieScore/index.tsx
+++ frontend/src/components/MovieScore/index.tsx
@@ -1,15 +1,18 @@
 import MovieStars from "components/MovieStars";
 import './styles.css';
 
-function MovieScore() {
+type Props = {
+  count: number;
+  score: number;
+}
 
-  const score = 3.5;
-  const count = 13;
+function MovieScore({count, score}: Props) {
+
   
   return (
     <div className="dsmovie-score-container">
       <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
-      <MovieStars />
+      <MovieStars score={score}/>
       <p className="dsmovie-score-count">{count} avaliações</p>
     </div>
   );
